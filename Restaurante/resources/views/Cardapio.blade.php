9032d0afc885b92885a0e2e47699761e10576b96
viniciusbassio <viniciusbassioa11@gmail.com>
2024-11-24 16:02:55 -0300

    Fix_Rotas_fornec/Nav_Bar melhorias de codigo e documentação

--- Restaurante/resources/views/Cardapio.blade.php
+++ Restaurante/resources/views/Cardapio.blade.php
@@ -12,7 +12,18 @@
     <title>Cardapio</title>
 </head>
 <body>
-<h1>Cardápio</h1>
+<div class="navbar">
+        <nav>
+            <ul>
+                <li><a href="/">Início</a></li>
+                <li><a href="/Cardapio">Cardápio</a></li>
+                <li><a href="/Cadastrar/Pedidos">Pedidos</a></li>
+                <li><a href="/fornecedores">Fornecedores </a></li>
+                <li><a href="/Relatorios">Relatorio</a></li>
+                <li><a href="/Cadatrar_Produtos">Produtos</a></li>
+            </ul>
+        </nav>
+    </div>
 <div class="container">
 
 <div class="row">
--- Restaurante/resources/views/relatorios.blade.php
+++ Restaurante/resources/views/relatorios.blade.php
@@ -11,12 +11,13 @@
     <div class="navbar">
         <nav>
             <ul>
-            <li><a href="/">Início</a></li>
+            <ul>
+                <li><a href="/">Início</a></li>
                 <li><a href="/Cardapio">Cardápio</a></li>
-                <li><a href="#">Reservas</a></li>
-                <li><a href="#">Estoque</a></li>
-                <li><a href="/fornecedores/cadastrar">Fornecedor</a></li>
+                <li><a href="/Cadastrar/Pedidos">Pedidos</a></li>
+                <li><a href="/fornecedores">Fornecedores </a></li>
                 <li><a href="/Relatorios">Relatorio</a></li>
+                <li><a href="/Cadatrar_Produtos">Produtos</a></li>
             </ul>
         </nav>
     </div>
